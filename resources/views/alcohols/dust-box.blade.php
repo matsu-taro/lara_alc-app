0ada5a59654be857752d82219d6fc0cc471ef5a3
matsu-taro <eee.rip179@gmail.com>
2024-01-20 23:44:54 +0900

    UI

--- resources/views/alcohols/dust-box.blade.php
+++ resources/views/alcohols/dust-box.blade.php
@@ -8,7 +8,8 @@
   <div class="create-btn">
     <a href="{{ route('alcohols.create') }}">
       <div class="create-btn-img">
-        <img src="{{ asset('storage/5.png') }}" alt="">
+        <img src="{{ asset('storage/5.png') }}" alt="" class="sp-none">
+        <img src="{{ asset('storage/18.png') }}" alt="" class="pc-none">
       </div>
       <p>新規作成</p>
     </a>
