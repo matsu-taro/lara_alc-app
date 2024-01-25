06e5a54412622cff59f7a0dd479a42c9d86acb5a
matsu-taro <eee.rip179@gmail.com>
2024-01-25 18:13:14 +0900

    UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;600&display=swap" rel="stylesheet">
 
   <!-- Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/style.scss'])
