7d0e700a63e04f77eea2a62fd970bdba34e324c9
matsu-taro <eee.rip179@gmail.com>
2024-01-28 19:28:02 +0900

    UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,16 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="{{ asset('storage/19.png') }}">
 
   <title>Laravel</title>
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
 
   @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/style.scss'])
   <!-- Styles -->
