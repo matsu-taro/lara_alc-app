bf64ac1326386e6fa5760c1b24881eb046b3ec8a
matsu-taro <eee.rip179@gmail.com>
2024-01-28 20:02:05 +0900

    UI

--- resources/views/alcohols/dust-box.blade.php
+++ resources/views/alcohols/dust-box.blade.php
@@ -1,19 +1,6 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Dashboard') }}
-    </h2>
-  </x-slot>
-
-  <div class="create-btn">
-    <a href="{{ route('alcohols.create') }}">
-      <div class="create-btn-img">
-        <img src="{{ asset('storage/5.png') }}" alt="" class="sp-none">
-        <img src="{{ asset('storage/18.png') }}" alt="" class="pc-none">
-      </div>
-      <p>新規作成</p>
-    </a>
-  </div>
+  
+  <x-create-btn />
 
   <x-sp-header />
 
--- resources/views/alcohols/index.blade.php
+++ resources/views/alcohols/index.blade.php
@@ -1,22 +1,9 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Dashboard') }}
-    </h2>
-  </x-slot>
 
-  <div class="create-btn">
-    <a href="{{ route('alcohols.create') }}">
-      <div class="create-btn-img">
-        <img src="{{ asset('storage/5.png') }}" alt="" class="sp-none">
-        <img src="{{ asset('storage/18.png') }}" alt="" class="pc-none">
-      </div>
-      <p>新規作成</p>
-    </a>
-  </div>
+  <x-create-btn />
 
   <x-sp-header />
-  
+
   <div class="serch-block">
     <div class="serch">
       <div class="serch-title">
@@ -98,127 +85,127 @@
     @endif
   </div>
 
-  <div class="">
+  <div>
     @if ($alcohols->count() == 0)
       <div class="empty-message">
         <p>リストは0件です。<br>新しく作りましょう！</p>
       </div>
     @else
-    <ul class="alc-cards">
-      @foreach ($alcohols as $alcohol)
-        <li class="alc-card" id="{{ $alcohol->id }}">
-          <div class="type-name">
-            @if ($alcohol->type == 1)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/10.png') }}" alt="">
-              </div>
-              <p>ビール</p>
-            @elseif($alcohol->type == 2)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/11.png') }}" alt="">
-              </div>
-              <p>サワー・酎ハイ</p>
-            @elseif($alcohol->type == 3)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/12.png') }}" alt="">
-              </div>
-              <p>ワイン</p>
-            @elseif($alcohol->type == 4)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/13.png') }}" alt="">
-              </div>
-              <p>日本酒</p>
-            @elseif($alcohol->type == 5)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/14.png') }}" alt="">
-              </div>
-              <p>焼酎</p>
-            @elseif($alcohol->type == 6)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/15.png') }}" alt="">
-              </div>
-              <p>洋酒</p>
-            @elseif($alcohol->type == 7)
-              <div class="type-name-icon">
-                <img src="{{ asset('storage/16.png') }}" alt="">
-              </div>
-              <p>その他</p>
-            @endif
-          </div>
-          <div class="alc-card-inner">
-            <div class="alc-card-inner-top">
-              <div class="image-area list">
-                <ul>
-                  @php
-                    $alcoholImages = $images->where('alcohol_id', $alcohol->id)->take(3); // 特定のアルコールに関連する画像を取得（最大3枚）
-                    $imageCount = count($alcoholImages);
-                  @endphp
+      <ul class="alc-cards">
+        @foreach ($alcohols as $alcohol)
+          <li class="alc-card" id="{{ $alcohol->id }}">
+            <div class="type-name">
+              @if ($alcohol->type == 1)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/10.png') }}" alt="">
+                </div>
+                <p>ビール</p>
+              @elseif($alcohol->type == 2)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/11.png') }}" alt="">
+                </div>
+                <p>サワー・酎ハイ</p>
+              @elseif($alcohol->type == 3)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/12.png') }}" alt="">
+                </div>
+                <p>ワイン</p>
+              @elseif($alcohol->type == 4)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/13.png') }}" alt="">
+                </div>
+                <p>日本酒</p>
+              @elseif($alcohol->type == 5)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/14.png') }}" alt="">
+                </div>
+                <p>焼酎</p>
+              @elseif($alcohol->type == 6)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/15.png') }}" alt="">
+                </div>
+                <p>洋酒</p>
+              @elseif($alcohol->type == 7)
+                <div class="type-name-icon">
+                  <img src="{{ asset('storage/16.png') }}" alt="">
+                </div>
+                <p>その他</p>
+              @endif
+            </div>
+            <div class="alc-card-inner">
+              <div class="alc-card-inner-top">
+                <div class="image-area list">
+                  <ul>
+                    @php
+                      $alcoholImages = $images->where('alcohol_id', $alcohol->id)->take(3); // 特定のアルコールに関連する画像を取得（最大3枚）
+                      $imageCount = count($alcoholImages);
+                    @endphp
 
-                  @foreach ($alcoholImages as $image)
-                    <li>
-                      <img src="{{ asset('storage/imgs/' . $image->original_file_name) }}" alt="">
+                    @foreach ($alcoholImages as $image)
+                      <li>
+                        <img src="{{ asset('storage/imgs/' . $image->original_file_name) }}" alt="">
+                      </li>
+                    @endforeach
+
+                    @for ($i = $imageCount; $i < 3; $i++)
+                      <li>
+                        <img src="{{ asset('storage/no-image.jpg') }}" alt="">
+                      </li>
+                    @endfor
+                  </ul>
+                </div>
+
+                <div class="alc-card-right">
+                  <ul>
+                    <li class="alc-card-detail">
+                      <p>名前</p>
+                      <span>{{ \Illuminate\Support\Str::limit($alcohol->alc_name, $limit = 22, $end = '...') }}</span>
                     </li>
-                  @endforeach
-
-                  @for ($i = $imageCount; $i < 3; $i++)
-                    <li>
-                      <img src="{{ asset('storage/no-image.jpg') }}" alt="">
+                    <li class="alc-card-detail">
+                      <p>値段</p>
+                      <span>¥{{ $alcohol->price }}</span>
                     </li>
-                  @endfor
-                </ul>
+                    <li class="alc-card-detail">
+                      <p>お店</p>
+                      <span>{{ \Illuminate\Support\Str::limit($alcohol->place, $limit = 22, $end = '...') }}</span>
+                    </li>
+                    <li class="alc-card-detail">
+                      <p>おいしさ</p>
+                      <span>
+                        @if ($alcohol->status == 1)
+                          うまい！
+                        @elseif($alcohol->status == 2)
+                          おいしい
+                        @elseif($alcohol->status == 3)
+                          まぁまぁかな
+                        @endif
+                      </span>
+                    </li>
+                  </ul>
+                </div>
               </div>
-
-              <div class="alc-card-right">
-                <ul>
-                  <li class="alc-card-detail">
-                    <p>名前</p>
-                    <span>{{ \Illuminate\Support\Str::limit($alcohol->alc_name, $limit = 22, $end = '...') }}</span>
-                  </li>
-                  <li class="alc-card-detail">
-                    <p>値段</p>
-                    <span>¥{{ $alcohol->price }}</span>
-                  </li>
-                  <li class="alc-card-detail">
-                    <p>お店</p>
-                    <span>{{ \Illuminate\Support\Str::limit($alcohol->place, $limit = 22, $end = '...') }}</span>
-                  </li>
-                  <li class="alc-card-detail">
-                    <p>おいしさ</p>
-                    <span>
-                      @if ($alcohol->status == 1)
-                        うまい！
-                      @elseif($alcohol->status == 2)
-                        おいしい
-                      @elseif($alcohol->status == 3)
-                        まぁまぁかな
-                      @endif
-                    </span>
-                  </li>
-                </ul>
+              <div class="alc-card-detail alc-card-detail-memo">
+                <p>メモ</p>
+                <span>{{ \Illuminate\Support\Str::limit($alcohol->memo, $limit = 40, $end = '...') }}</span>
               </div>
             </div>
-            <div class="alc-card-detail alc-card-detail-memo">
-              <p>メモ</p>
-              <span>{{ \Illuminate\Support\Str::limit($alcohol->memo, $limit = 40, $end = '...') }}</span>
+            <div class="alc-card-bottom">
+              <form action="{{ route('alcohols.destroy', ['alcohol' => $alcohol->id]) }}" method="POST">
+                @method('delete')
+                @csrf
+                <button>
+                  ゴミ箱へ
+                </button>
+              </form>
+              <div>
+                <a href="{{ route('alcohols.edit', ['alcohol' => $alcohol->id]) }}" class="">
+                  編集する
+                </a>
+              </div>
             </div>
-          </div>
-          <div class="alc-card-bottom">
-            <form action="{{ route('alcohols.destroy', ['alcohol' => $alcohol->id]) }}" method="POST">
-              @method('delete')
-              @csrf
-              <button>
-                ゴミ箱へ
-              </button>
-            </form>
-            <div>
-              <a href="{{ route('alcohols.edit', ['alcohol' => $alcohol->id]) }}" class="">
-                編集する
-              </a>
-            </div>
-          </div>
-        </li>
-      @endforeach
-    </ul>
+          </li>
+        @endforeach
+      </ul>
     @endif
     {{ $alcohols->links() }}
   </div>
